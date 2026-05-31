4672ba23ab761ccdaa5f268ee0fb901c2f9ea5b9
skibididopdopdopdopyesyes <skibididopdopdopdopyesyes0@gmail.com>
2026-05-31 13:50:02 +0300

    Create MixinClientPlayerInteractionManager.java

---
+++ src/main/java/com/swill/killaura/mixin/MixinClientPlayerInteractionManager.java
@@ -0,0 +1,18 @@
+package com.swill.killaura.mixin;
+
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.swill.killaura.RotationHandler;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class MixinClientPlayerInteractionManager {
+    @Inject(method = "attackEntity", at = @At("HEAD"))
+    private void onAttackEntity(PlayerEntity player, Entity target, CallbackInfo ci) {
+        RotationHandler.applySilentRotation();
+    }
+}
